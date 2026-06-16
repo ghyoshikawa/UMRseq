3f85c76c653659537cb242658a63dd532859a588
Guilherme Pedro Ventura Yoshicawa Simoes Silva <ghyoshikawa@gmail.com>
2026-06-16 16:59:12 +1000

    ...

--- chrSizes.sh
+++ chrSizes.sh
@@ -2,18 +2,18 @@
 #SBATCH --partition=general
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20            #change for multi-threaded jobs
-#SBATCH --time=1-00:00:00             # time allocation (D-HH:MM:SS)
-#SBATCH --mem=100G                   # memory pool for all cores
+#SBATCH --cpus-per-task=1           #change for multi-threaded jobs
+#SBATCH --time=00:10:00             # time allocation (D-HH:MM:SS)
+#SBATCH --mem=5G                   # memory pool for all cores
 #SBATCH --qos=normal
 #SBATCH --account=a_agfs_ps
-#SBATCH --job-name=buildBowtieIndex           # sensible name for the job
+#SBATCH --job-name=chrSizes           # sensible name for the job
 #SBATCH -o ./out/index.%j.out             # output file
 #SBATCH -e ./out/index.%j.err             # error file
 #SBATCH --mail-type=END		        # Notifications (once the code words, change to END to avoid too many emails)
 #SBATCH --mail-user=g.vyoshikawa@uq.edu.au
 
-module load bowtie2/2.5.1-gcc-12.3.0 samtools/1.18-gcc-12.3.0
+module load samtools/1.18-gcc-12.3.0
 
 cd /home/uqgventu/UMR_sorghum/genome/v5.1/assembly
  if [ -f Sbicolor_730_v5.0.fa.gz ]; then
@@ -24,17 +24,6 @@ cd /home/uqgventu/UMR_sorghum/genome/v5.1/assembly
    else
      echo "No genome found"
 fi
-
-#use --large-index if the genome is larger than 4GB
-bowtie2-build --threads 20 Sbicolor_730_v5.0.fa Sbicolor_730_v5.0
-if [ $? -ne 0 ]; then
-        echo "Failed to build index for Sbicolor_730_v5.0.fa"
-        exit 1
-    else
-        echo "Index built successfully for Sbicolor_730_v5.0.fa"
-    fi
-
-bowtie2-inspect -vs Sbicolor_730_v5.0 > Sbicolor_730_v5.0_index_info.txt
 
 #Define chromosome sizes
 samtools faidx Sbicolor_730_v5.0.fa
